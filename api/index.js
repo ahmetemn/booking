f5b91078a2310081ee281f4bc37e966b650b152c
antares <antares@cipherbiz.com>
2023-02-03 00:01:15 +0300

    add router

--- api/index.js
+++ api/index.js
@@ -2,34 +2,30 @@ import express from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 mongoose.set('strictQuery', true)
-const app =express();
+const app = express();
 dotenv.config()
- 
 
-const connect = async() =>{
-    try{
+
+const connect = async () => {
+    try {
         await mongoose.connect(process.env.MONGO);
-        console.log("MongoDb Connected Succesful")
-    }catch(err){
-
-        console.log("MongoDb didnt connect "); 
-
-        throw error 
+    } catch (err) {
+        throw error
 
     }
 }
 
-mongoose.connection.on('connected' , () =>{
+mongoose.connection.on('connected', () => {
     console.log("mongoDb connected :) ");
 });
 
-mongoose.connection.on ('disconnected' , () =>{
+mongoose.connection.on('disconnected', () => {
     console.log("mongoDb disconnected !")
 })
 
 
 
-app.listen(8800 , ()=>{
+app.listen(8800, () => {
     connect()
 })
 
