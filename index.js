22f16d461a30a1a6cd26b605b0c2e8b45ea7b3b9
antares <antares@cipherbiz.com>
2023-02-02 23:37:25 +0300

    add MongoDb connected and create .env files

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+mongoose.set('strictQuery', true)
 const app =express();
+dotenv.config()
+ 
+
+const connect = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("MongoDb Connected Succesful")
+    }catch(err){
+
+        console.log("MongoDb didnt connect "); 
+
+        throw error 
+
+    }
+}
+
 
 app.listen(8800 , ()=>{
-    console.log("Connecto to backend")
+    connect()
 })
 
